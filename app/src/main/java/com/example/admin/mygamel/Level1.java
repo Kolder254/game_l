889a11d8a9d36d5956faa7126a272e2107e6f1c9
Your Name <you@example.com>
2017-05-29 20:58:45 +0300

    new commit

--- app/src/main/java/com/example/admin/mygamel/Level1.java
+++ app/src/main/java/com/example/admin/mygamel/Level1.java
@@ -31,7 +31,7 @@ public class Level1 extends Level implements View.OnClickListener{
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         arrayList = new ArrayList<>();
-        //
+        int i  = 5;
         ImageView view11 = (ImageView)getView().findViewById(R.id.pos_1_1);
         ElementCorner view12 = (ElementCorner)getView().findViewById(R.id.pos_1_2);
         ElementCorner view13 = (ElementCorner)getView().findViewById(R.id.pos_1_3);
